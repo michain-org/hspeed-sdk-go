7587f896eb2820d04a7f24c6d8ef7465d4afefc3
hdongZ <949077032@qq.com>
2020-04-22 13:53:26 +0800

    test signer type

--- pkg/client/resmgmt/lifecycle.go
+++ pkg/client/resmgmt/lifecycle.go
@@ -1,5 +1,12 @@
 package resmgmt
 
+import (
+	pb "github.com/hyperledger/fabric-protos-go/peer"
+	"github.com/michain-org/hspeed-sdk-go/internal/github.com/hyperledger/fabric/common/cauthdsl"
+	"github.com/michain-org/hspeed-sdk-go/internal/github.com/hyperledger/fabric/protoutil"
+	"github.com/pkg/errors"
+)
+
 const (
 	lifecycleName                   = "_lifecycle"
 	installFuncName                 = "InstallChaincode"
@@ -11,3 +18,40 @@ const (
 	queryChaincodeFuncName          = "QueryChaincodeDefinition"
 	queryChaincodesFuncName         = "QueryChaincodeDefinitions"
 )
+
+func createPolicyBytes(signaturePolicy, channelConfigPolicy string) ([]byte, error) {
+	if signaturePolicy == "" && channelConfigPolicy == "" {
+		// no policy, no problem
+		return nil, nil
+	}
+
+	if signaturePolicy != "" && channelConfigPolicy != "" {
+		// mo policies, mo problems
+		return nil, errors.New("cannot specify both \"--signature-policy\" and \"--channel-config-policy\"")
+	}
+
+	var applicationPolicy *pb.ApplicationPolicy
+	if signaturePolicy != "" {
+		signaturePolicyEnvelope, err := cauthdsl.FromString(signaturePolicy)
+		if err != nil {
+			return nil, errors.Errorf("invalid signature policy: %s", signaturePolicy)
+		}
+
+		applicationPolicy = &pb.ApplicationPolicy{
+			Type: &pb.ApplicationPolicy_SignaturePolicy{
+				SignaturePolicy: signaturePolicyEnvelope,
+			},
+		}
+	}
+
+	if channelConfigPolicy != "" {
+		applicationPolicy = &pb.ApplicationPolicy{
+			Type: &pb.ApplicationPolicy_ChannelConfigPolicyReference{
+				ChannelConfigPolicyReference: channelConfigPolicy,
+			},
+		}
+	}
+
+	policyBytes := protoutil.MarshalOrPanic(applicationPolicy)
+	return policyBytes, nil
+}
